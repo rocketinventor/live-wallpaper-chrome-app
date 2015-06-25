216868557b76233aeb2d9f231e41e62132603cf2
Elliot <emgerchak@gmail.com>
2015-06-25 18:10:24 -0400

    Time scapes live wallpaper from Chromebook Pixel

--- background.js
+++ background.js
@@ -28,7 +28,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
       var video = document.createElement('video');
       video.autoplay = true;
-      video.src = URL.createObjectURL(localStream);
+      video.src = "/TimeScapes.mp4";
       video.addEventListener('loadedmetadata', function() {
   
         var canvas = document.createElement('canvas');
